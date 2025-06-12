42f431296b1ba63633e59e5f1f29527299ad729f
akshaym95 <akshay.yem.95@gmail.com>
2025-06-12 15:04:48 +0530

    added code to handle the different delimitor scenario

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -8,10 +8,17 @@ class Calculator
     else
       if str_input[0] != "/"
         numbers = str_input.split /\s|\n|,/
-        sum = 0
-        numbers.each { |num| sum+= num.to_i }
-        sum
+        sum(numbers)
+      else
+        new_delimitor = str_input[2]
+        numbers = str_input.split("\n").last.split(new_delimitor)
+        sum(numbers)
       end
     end
+  end
+  def sum(numbers)
+    sum = 0
+    numbers.each { |num| sum+= num.to_i }
+    sum
   end
 end
